6a99b08b749501819c9ee121e96191b0454f34b4
ruslan.sibgatulin05@gmail.com <ruslan.sibgatulin05@gmail.com>
2021-10-06 20:16:11 +0600

    added button and logic

--- app/src/main/java/com/example/todolistapp/LogInActivity.kt
+++ app/src/main/java/com/example/todolistapp/LogInActivity.kt
@@ -1,11 +1,30 @@
 package com.example.todolistapp
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.example.todolistapp.databinding.ActivityLogInBinding
 
 class LogInActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLogInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+
+        binding.logInBtn.setOnClickListener {
+            if(binding.logInEdt.text.toString().isNotEmpty()) {
+                    Intent(this, MainActivity::class.java).also {
+                        it.putExtra("EXTRA_NAME", binding.logInEdt.text.toString())
+                        startActivity(it)
+                    }
+            } else {
+                Toast.makeText(this, "Wrong!", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        setContentView(binding.root)
     }
 }
